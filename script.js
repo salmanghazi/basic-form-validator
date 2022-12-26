3532b1d3be99577aaa028ae91ea57631d05f1172
Muhammad Salman <muhammad.salman@keeptrucki.com>
2022-12-26 22:19:02 +0500

    Completed basic validation

--- script.js
+++ script.js
@@ -4,13 +4,53 @@ const email = document.getElementById('email');
 const password = document.getElementById('password');
 const confirmPassword = document.getElementById('password2');
 
+function displaySuccess(inputField) {
+  const formControl = inputField.parentElement;
+  formControl.className = 'form-control success';
+}
+
+function displayError(inputField, message) {
+  const formControl = inputField.parentElement;
+  formControl.className = 'form-control error';
+  const smallTag = formControl.querySelector('small');
+  smallTag.innerText = message;
+}
+
+function isEmailValid(email) {
+  return email.match(
+    /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+  );
+}
+
 form.addEventListener('submit', (e) => {
   e.preventDefault();
 
-  if (username.value === '') alert('Username is required');
-  else if (email.value === '') alert('Email is required');
-  else if (password.value === '') alert('Password is required');
-  else if (confirmPassword.value === '') alert('Renter the password to confirm');
+  if (username.value === '') {
+    displayError(username, 'Username is required');
+  } else {
+    displaySuccess(username);
+  }
 
-  
-})
+
+  if (email.value === '') {
+    displayError(email, 'Email is required');
+  } else if (!isEmailValid(email.value)) {
+    displayError(email, 'Email is invalid');
+  } else {
+    displaySuccess(email);
+  }
+
+
+  if (password.value === '') {
+    displayError(password, 'Password is required');
+  } else {
+    displaySuccess(password);
+  }
+
+
+  if (confirmPassword.value === '') {
+    displayError(confirmPassword, 'Please confirm password');
+  } else {
+    displaySuccess(confirmPassword);
+  }
+});
